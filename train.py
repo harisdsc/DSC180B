db16c697fa66f08f2ca8e52e9644d30c43c63d37
hari3mp <harisasaif@gmail.com>
2026-02-12 10:38:22 -0800

    train

--- train.py
+++ train.py
@@ -14,10 +14,12 @@ def train_model(model):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     if model == 'log-reg':
+        print('Training Logistic Regression model...')
         model = LogisticRegression()
         model.fit(X_train, y_train)
 
     elif model == 'xgboost':
+        print('Training XGBoost model...')
         model = xgb.XGBClassifier()
         model.fit(X_train, y_train)
 
@@ -36,14 +38,22 @@ def train_model(model):
     test_preds = model.predict_proba(X_test)[:, 1]
     test_score = roc_auc_score(y_test, test_preds)
 
-    print(f"Training AUC: {train_score}")
-    print(f"Testing AUC: {test_score}")
+    print(f'Training AUC: {train_score}')
+    print(f'Testing AUC: {test_score}')
     return model
 
 if __name__ == "__main__":
     args = sys.argv
     if len(args) > 1:
-        model = args[1]
+        if args[1] == 'all':
+            print('-----------------------------')
+            for model in ['log-reg', 'xgboost', 'catboost']:
+                print(f'{model}:')
+                train_model(model)
+                print('-----------------------------')
+            sys.exit(0)
+        else:
+            model = args[1]
     else:
         model = 'log-reg'
     train_model(model)
