085ad4c0ff07ac2f09cc7e1f26b94b4c2dd6f732
hari3mp <harisasaif@gmail.com>
2026-02-14 06:31:06 -0800

    tuning

--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 from src.pipeline import run_pipeline
+from src.tuning import tune_with_optuna
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split, RandomizedSearchCV, StratifiedKFold
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
-from scipy.stats import uniform, randint, loguniform
 from catboost import CatBoostClassifier
 from catboost.utils import get_gpu_device_count
-import numpy as np
+import pandas as pd
 import xgboost as xgb
 import lightgbm as lgb
 import time
@@ -15,7 +15,6 @@ import json
 import sys
 
 MODELS = ['log-reg', 'xgboost', 'catboost', 'lightgbm']
-
 GPU_AVAILABLE = True if get_gpu_device_count() > 0 else False
 
 def train_model(model_name, df, tune=False):
@@ -23,53 +22,37 @@ def train_model(model_name, df, tune=False):
 
     X = df.drop(columns=['prism_consumer_id', 'DQ_TARGET', 'evaluation_date'])
     y = df['DQ_TARGET']
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+    scores = pd.DataFrame(columns=['model', 'train_auc', 'test_auc', 'train_time', 'tune_time', 'date'])
 
-    if model_name == 'log-reg':
-        model = Pipeline([
-            ('scaler', StandardScaler()), 
-            ('clf', LogisticRegression(max_iter=1000))
-        ])
-    elif model_name == 'xgboost':
-        model = xgb.XGBClassifier(eval_metric='auc')
-    elif model_name == 'catboost':
-        model = CatBoostClassifier(verbose=0, allow_writing_files=False)
-    elif model_name == 'lightgbm':
-        model = lgb.LGBMClassifier(verbose=-1)
-    else:
-        print('Invalid model.')
-        return 0
-
-    if tune:
+    if tune and model_name in MODELS:
         tune_start = time.time()
-        print(f"{time.strftime("%I:%M %p")} - Starting hyperparameter search for {model_name}...")
-        param_dist = get_params(model_name)
-        
-        cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
-        search = RandomizedSearchCV(
-            estimator=model,
-            param_distributions=param_dist,
-            n_iter=20,
-            scoring='roc_auc',
-            cv=cv,
-            verbose=1,
-            random_state=42,
-            n_jobs=-1
-        )
-        
-        search.fit(X_train, y_train)
-        print(f"Best Params: {search.best_params_}")
-        print(f"Best CV AUC: {search.best_score_}")
-        print(f"Hyperparameter search time: {time.time() - tune_start} seconds.")
-        model = search.best_estimator_
+        model, best_params = tune_with_optuna(model_name, X_train, y_train, n_trials=30)
 
         with open(f"configs/{model_name}.json", "w") as f:
-            json.dump(search.best_params_, f)
+            json.dump(best_params, f, indent=4)
+
+        print(f"Hyperparameter search time: {time.time() - tune_start:.2f} seconds.")
         
     else:
         print(f"Training {model_name} with default parameters...")
-        model.fit(X_train, y_train)
+
+        if model_name == 'log-reg':
+            model = Pipeline([
+                ('scaler', StandardScaler()), 
+                ('clf', LogisticRegression(max_iter=25_000, class_weight='balanced'))
+            ])
+        elif model_name == 'xgboost':
+            model = xgb.XGBClassifier(eval_metric='auc')
+        elif model_name == 'catboost':
+            model = CatBoostClassifier(verbose=0, allow_writing_files=False)
+        elif model_name == 'lightgbm':
+            model = lgb.LGBMClassifier(verbose=-1)
+        else:
+            print('Invalid model.')
+            return 0
+
+    model.fit(X_train, y_train)
 
     train_preds = model.predict_proba(X_train)[:, 1]
     train_score = roc_auc_score(y_train, train_preds)
@@ -79,45 +62,15 @@ def train_model(model_name, df, tune=False):
 
     print(f'Training AUC: {train_score}')
     print(f'Testing AUC:  {test_score}')
-    print(f"Total time: {time.time() - model_start} seconds.")
+    print(f"Total time: {time.time() - model_start:.2f} seconds.")
+
+    scores = scores.append({'model': model_name, 'train_auc': train_score, 
+                            'test_auc': test_score, 'train_time': time.time() - model_start, 
+                            'tune_time': time.time() - tune_start if tune else 0,
+                            'date': pd.Timestamp.now()}, ignore_index=True)
+    scores.to_csv('model_scores.csv', index=False)
 
     return model
-
-def get_params(model_name):
-    if model_name == 'log-reg':
-            return {
-                'clf__C': loguniform(1e-4, 100),
-                'clf__penalty': ['l2'],
-                'clf__solver': ['lbfgs', 'liblinear']
-            }
-    elif model_name == 'xgboost':
-        return {
-            'n_estimators': randint(100, 1000),
-            'max_depth': randint(3, 10),
-            'learning_rate': loguniform(0.01, 0.3),
-            'subsample': uniform(0.6, 0.4),
-            'colsample_bytree': uniform(0.6, 0.4),
-            'min_child_weight': randint(1, 10),
-            'gamma': uniform(0, 0.5)
-        }
-    elif model_name == 'catboost':
-        return {
-            'iterations': randint(100, 1000),
-            'depth': randint(4, 10),
-            'learning_rate': loguniform(0.01, 0.3),
-            'l2_leaf_reg': uniform(1, 10),
-            'border_count': randint(32, 255)
-        }
-    elif model_name == 'lightgbm':
-        return {
-            'n_estimators': randint(100, 1000),
-            'num_leaves': randint(20, 150),
-            'learning_rate': loguniform(0.01, 0.3),
-            'subsample': uniform(0.6, 0.4),
-            'colsample_bytree': uniform(0.6, 0.4),
-            'min_child_samples': randint(10, 100)
-        }
-    return {}
 
 if __name__ == "__main__":
     total_start = time.time()
@@ -135,11 +88,11 @@ if __name__ == "__main__":
 
     if model == 'all':
         print('-----------------------------')
-        for model in MODELS:
-            print(f'Processing {model}...')
-            train_model(model, df, tune=tune)
+        for m in MODELS:
+            print(f'Processing {m}...')
+            train_model(m, df, tune=tune)
             print('-----------------------------')
     else:
         train_model(model, df, tune=tune)
 
-    print(f"Total time: {time.time() - total_start} seconds.")
+    print(f"Total Execution time: {time.time() - total_start:.2f} seconds.")
